c4bee366ab6f819adf0b78d3598cf6eca308a145
Emory Merryman <emory.merryman@gmail.com>
2017-12-16 03:08:50 +0000

    use /opt/docker/workspace not /workspace

--- root/run.user.sh
+++ root/run.user.sh
@@ -5,4 +5,4 @@ mkdir /home/user/.ssh &&
     touch /home/user/.ssh/config /home/user/.ssh/known_hosts /home/user/.ssh/upstream_id_rsa /home/user/.ssh/origin_id_rsa /home/user/.ssh/report_id_rsa &&
     chmod 0600 /home/user/.ssh/upstream_id_rsa /home/user/.ssh/origin_id_rsa /home/user/.ssh/report_id_rsa /home/user/.ssh/config &&
     chmod 0644 /home/user/.ssh/known_hosts &&
-    git -C /workspace init
+    git -C /opt/docker/workspace init
