29d712ba57d7133f51573fa5f5f66fbdf01ec7d6
Emory Merryman <emory.merryman@gmail.com>
2017-12-17 02:46:17 +0000

    fix scratch script

--- root/bin/git-scratch.sh
+++ root/bin/git-scratch.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
-project-status &&
+while [ ${#} -gt 0 ]
+do
+    case ${1} in
+        --message)
+            MESSAGE="${2}" &&
+                shift 2
+        ;;
+        --std-in)
+            MESSAGE_FILE=$(mktemp) &&
+                shift
+        ;;
+    esac
+done &&
+    project-status &&
     git fetch upstream "${MASTER_BRANCH}" &&
     git checkout "upstream/${MASTER_BRANCH}" &&
     git checkout -b scratch/$(uuidgen) &&
-    COMMIT_MESSAGE_FILE=$(mktemp) &&
-    tee ${COMMIT_MESSAGE_FILE} &&
-    if [ ! -z "$(cat ${COMMIT_MESSAGE_FILE})" ]
+    if [ -z "${MESSAGE}" ] && [ -z "${MESSAGE_FILE}" ]
     then
-        git commit --allow-empty --file ${COMMIT_MESSAGE_FILE}
-    fi &&
-    rm -f ${COMMIT_MESSAGE_FILE}
+        git commit --allow-empty
+    elif [ -z "${MESSAGE}" ]
+    then
+        tee ${MESSAGE_FILE} &&
+            git commit --allow-empty --file ${MESSAGE_FILE} &&
+            rm -f ${MESSAGE_FILE}
+    else
+        git commit --allow-empty --message "${MESSAGE}"
+    fi
