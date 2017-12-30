c3dcb24a48cb102deaac20c7f07564416e18a9b5
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 11:49:59 +0000

    rework the refresh script.  it adds an empty commit message at the end saying success or failure and exits 64 on failure

--- root/bin/git-refresh.sh
+++ root/bin/git-refresh.sh
@@ -2,10 +2,15 @@
 
 git standing &&
     git fetch upstream "${MASTER_BRANCH}" &&
+    THIS_COMMIT=$(git rev-parse HEAD) &&
+    MASTER_COMMIT=$(git rev-parse "upstream/${MASTER_BRANCH}") &&
+    BASE_COMMIT=$(git merge-base ${THIS_COMMIT} ${MASTER_COMMIT}) &&
     git checkout -b scratch/$(uuidgen) &&
-    if [ ! git rebase "upstream/${MASTER_BRANCH}" ]
+    if [ git rebase "upstream/${MASTER_BRANCH}" ]
     then
+        git commit --allow-empty --message "Sucessfully rebased upstream/${MASTER_BRANCH}: THIS_COMMIT=${THIS_COMMIT}, MASTER_COMMIT=${MASTER_COMMIT}, BASE_COMMIT=${BASE_COMMIT}"
+    else
         git rebase --abort &&
-            git checkout --patch "upstream/${MASTER_BRANCH}" &&
-            git refresh
+            git commit -am --allow-empty --message "Failed to rebase upstream/${MASTER_BRANCH}: THIS_COMMIT=${THIS_COMMIT}, MASTER_COMMIT=${MASTER_COMMIT}, BASE_COMMIT=${BASE_COMMIT}" &&
+            exit 64
     fi
