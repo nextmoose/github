303cf76bc836baa686db40945fe84789fd3d81d0
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 11:54:28 +0000

    git prepare uses git refresh

--- root/bin/git-prepare.sh
+++ root/bin/git-prepare.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-git standing &&
-    git fetch upstream "${MASTER_BRANCH}" &&
+git refresh &&
     git checkout -b scratch/$(uuidgen) &&
     git reset --soft "upstream/${MASTER_BRANCH}" &&
-    git commit --reedit-message $(git log --pretty=format:"%h" master..$(git rev-parse HEAD) | tail -n 1)
+    git commit --reedit-message $(git log --pretty=format:"%h" "upstream/${MASTER_BRANCH}"..$(git rev-parse HEAD) | tail -n 1)
--- root/bin/git-refresh.sh
+++ root/bin/git-refresh.sh
@@ -6,11 +6,12 @@ git standing &&
     MASTER_COMMIT=$(git rev-parse "upstream/${MASTER_BRANCH}") &&
     BASE_COMMIT=$(git merge-base ${THIS_COMMIT} ${MASTER_COMMIT}) &&
     git checkout -b scratch/$(uuidgen) &&
-    if [ git rebase "upstream/${MASTER_BRANCH}" ]
+    if git rebase "upstream/${MASTER_BRANCH}"
     then
         git commit --allow-empty --message "Sucessfully rebased upstream/${MASTER_BRANCH}: THIS_COMMIT=${THIS_COMMIT}, MASTER_COMMIT=${MASTER_COMMIT}, BASE_COMMIT=${BASE_COMMIT}"
     else
         git rebase --abort &&
             git commit -am --allow-empty --message "Failed to rebase upstream/${MASTER_BRANCH}: THIS_COMMIT=${THIS_COMMIT}, MASTER_COMMIT=${MASTER_COMMIT}, BASE_COMMIT=${BASE_COMMIT}" &&
+            echo Failed to rebase &&
             exit 64
     fi
