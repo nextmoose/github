eef5837040745797e8eb1c3bbc201514777be2c4
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 18:14:32 +0000

    make the user add files before we commit

--- root/bin/git-prepare.sh
+++ root/bin/git-prepare.sh
@@ -5,6 +5,18 @@ then
     git checkout -b scratch/$(uuidgen) &&
         COMMENT_COMMIT=$(git log --pretty=format:"%h" "upstream/${MASTER_BRANCH}"..HEAD | tail -n 1) &&
         git refresh --soft "upstream/${MASTER_BRANCH}" &&
+        while ! git diff --exit-code
+        do
+            echo There are local unstaged changes. &&
+                git diff &&
+                bash
+        done &&
+        while [ ! -z "$(git ls-files --other --exclude-standard --directory --exclude .c9)" ]
+        do
+            echo There are untracked files that are not .gitignored - nor are they part of .c9. &&
+                git ls-files --other --exclude-standard --directory --exclude .c9 &&
+                bash
+        done &&
         git commit --reedit-message ${COMMENT_COMMIT}
 else
     echo Failed to cleanly refresh &&
