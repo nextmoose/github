4dfdd4d8c70a957d35e127a879b96db836dbda76
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 17:00:23 +0000

    fix post-commit script

--- root/bin/post-commit.sh
+++ root/bin/post-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-while ! git push origin $(git rev-parse --abbrev-ref HEAD)
+while ! git push --follow-tags origin $(git rev-parse --abbrev-ref HEAD)
 do
     sleep 10s
 done &&
-    project-status
+    git standing
